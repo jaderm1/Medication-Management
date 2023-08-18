ffe2da79c9ff6fde5fb854bd0ac7e10200df0ac2
Jaderm1 <jadermaciel1@gmail.com>
2023-08-18 01:22:02 -0300

    map page screen created

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,12 +1,32 @@
+import { MainStyled } from "../components/Main";
 import { Menu } from "../components/Menu";
+import { MapContainer, TileLayer, useMap,Marker,Popup } from 'react-leaflet'
+
+
+
 
 function Map() {
+  
 return(
 
   <>
-  <Menu></Menu>
-  <h1>Map</h1>
-  
+  <Menu/>
+
+    
+<MainStyled>
+<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ width:'100%', height: "100%" }}>
+<TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  /> 
+  <Marker position={[51.505, -0.09]}>
+  <Popup>
+    A pretty CSS3 popup. <br /> Easily customizable.
+  </Popup>
+</Marker>
+  </MapContainer>
+</MainStyled>
+
   </>
 )
 
