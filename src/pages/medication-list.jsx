a5e514793465452e6fb62ce44360e3be2b244779
Jaderm1 <jadermaciel1@gmail.com>
2023-08-23 22:35:43 -0300

    created medication list base page and set routes

---
+++ src/pages/medication-list.jsx
@@ -0,0 +1,18 @@
+import { Menu } from "../components/Menu";
+import { MainStyled } from "../components/Main";
+
+
+
+function MedicationList(){
+    return(
+        <>
+
+            <Menu/>
+              <MainStyled>
+            </MainStyled>
+        </>
+    )
+
+}
+
+export {MedicationList};
