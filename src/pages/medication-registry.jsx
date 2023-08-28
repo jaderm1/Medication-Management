9cf97bb2c828ad8e8956c92843c683d6d4fdb22b
Jader Luiz Maciel do Nascimento <109469595+jaderm1@users.noreply.github.com>
2023-08-28 19:49:55 -0300

    fix local storage

--- src/pages/medication-registry.jsx
+++ src/pages/medication-registry.jsx
@@ -1,199 +1,215 @@
 import { Menu } from "../components/Menu";
 import { MainStyled } from "../components/Main";
 import { TextField } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import MenuItem from '@mui/material/MenuItem';
 
 
 
-function MedicationRegistry (){
-const [medicationName, setMedicationName] = useState("")
-const [medicationNameError, setMedicationNameError] = useState("")
-const [laboratoryName, setLaboratoryName] = useState("")
-const [laboratoryNameError, setLaboratoryNameError] = useState("")
-const [dosage, setDosage] = useState("")
-const [dosageError, setDosageError] = useState("")
-const [quantity, setQuantity] = useState("")
-const [quantityError, setQuantityError] = useState("")
-const [price, setPrice] = useState("")
-const [priceError, setPriceError] = useState("")
-const [description, setDescription] = useState("")
-const [type, setType] = useState("")
+function MedicationRegistry() {
+  const [medicationName, setMedicationName] = useState("")
+  const [laboratoryName, setLaboratoryName] = useState("")
+  const [dosage, setDosage] = useState("")
+  const [quantity, setQuantity] = useState("")
+  const [quantityError, setQuantityError] = useState("")
+  const [price, setPrice] = useState("")
+  const [description, setDescription] = useState("")
+  const [type, setType] = useState("")
+  const [medicationList, setMedicationList] = useState([]);
 
 
 
-const handleMedicationName = (e) => {
-  setMedicationName(e.target.value)
-}
-const handleLaboratoryName = (e) => {
-  setLaboratoryName(e.target.value)
-}
-const handleDosage = (e) => {
-  setDosage(e.target.value)
-}
-const handleQuantity = (e) => {
-  setQuantity(e.target.value)
-  if(isNaN(e.target.value)){
-    setQuantityError("Apenas números")
+  useEffect(() => {
+    const storedList = localStorage.getItem("List");
+    if (storedList) {
+      setMedicationList(JSON.parse(storedList));
+    }
+  }, []);
+
+
+
+  const handleMedicationName = (e) => {
+    setMedicationName(e.target.value)
   }
-    else{
+  const handleLaboratoryName = (e) => {
+    setLaboratoryName(e.target.value)
+  }
+  const handleDosage = (e) => {
+    setDosage(e.target.value)
+  }
+  const handleQuantity = (e) => {
+    setQuantity(e.target.value)
+    if (isNaN(e.target.value)) {
+      setQuantityError("Apenas números")
+    }
+    else {
       setQuantityError("")
     }
-}
-const handlePrice = (e) => {
-  setPrice(e.target.value)
-}
-const handleQuantityError = (e) => {
-  setQuantityError(e.target.value)
-}
-const handleDescription = (e) => {
+  }
+  const handlePrice = (e) => {
+    setPrice(e.target.value)
+  }
+ 
+  const handleDescription = (e) => {
     setDescription(e.target.value)
-    }
-const handleType = (e) => {
+  }
+  const handleType = (e) => {
     setType(e.target.value)
-    }
+  }
 
-const handleSubmit = (e) => {
-  e.preventDefault()
+  const handleSubmit = (e) => {
 
-  alert("Medicamento cadastrado com sucesso")
+    const inputs = {}
 
-  localStorage.setItem("medicationName",JSON.stringify(medicationName))
-    localStorage.setItem("laboratoryName",JSON.stringify(laboratoryName))
-    localStorage.setItem("dosage",JSON.stringify(dosage))
-    localStorage.setItem("quantity",JSON.stringify(quantity))
-    localStorage.setItem("price",JSON.stringify(price))
-    localStorage.setItem("description",JSON.stringify(description))
-    localStorage.setItem("type",JSON.stringify(type))
+    inputs.medicationName = medicationName
+    inputs.laboratoryName = laboratoryName
+    inputs.dosage = dosage
+    inputs.quantity = quantity
+    inputs.price = price
+    inputs.description = description
+    inputs.type = type
+    inputs.id = medicationList.length + 1
+
+
+    setMedicationList([...medicationList, inputs]);
+
+
+    localStorage.setItem("List", JSON.stringify([...medicationList, inputs]));
+
+    
     
 
-}
+    alert("Medicamento cadastrado com sucesso")
+
+    e.preventDefault()
+
+
+   
+
+
+  }
 
 
 
 
-const inputFieldsRequired = [{
-  label: 'Nome do Medicamento',
-  name: 'medicationName',
-  id: 'medicationName',
-  value: medicationName,
-  handle: handleMedicationName,
-  error: medicationNameError
-},
-{
-label:'Nome do Laboratório',
-name:'laboratoryName',
-id:'laboratoryName',
-value:laboratoryName,
-handle:handleLaboratoryName,
-error:laboratoryNameError
+  const inputFieldsRequired = [{
+    label: 'Nome do Medicamento',
+    name: 'medicationName',
+    id: 'medicationName',
+    value: medicationName,
+    handle: handleMedicationName,
+  },
+  {
+    label: 'Nome do Laboratório',
+    name: 'laboratoryName',
+    id: 'laboratoryName',
+    value: laboratoryName,
+    handle: handleLaboratoryName,
 
-}, {
-  label: 'Dosagem',
-  name: 'dosage',
-  id: 'dosage',
-  value: dosage,
-  handle: handleDosage,
-  error: dosageError
-}, {
-  label: 'Quantidade',
-  name: 'quantity',
-  id: 'quantity',
-  value: quantity,
-  handle: handleQuantity,
-  error : quantityError
+  }, {
+    label: 'Dosagem',
+    name: 'dosage',
+    id: 'dosage',
+    value: dosage,
+    handle: handleDosage,
+  }, {
+    label: 'Quantidade',
+    name: 'quantity',
+    id: 'quantity',
+    value: quantity,
+    handle: handleQuantity,
+    error: quantityError
 
-},{
-  label:'Preço',
-  name:'price',
-  id:'price',
-  value:price,
-  handle:handlePrice,
-  error:priceError
+  }, {
+    label: 'Preço',
+    name: 'price',
+    id: 'price',
+    value: price,
+    handle: handlePrice,
 
-}]
+  }]
 
-const typeMedications = [
+  const typeMedications = [
     {
-        value: 'generico',
-        label: 'Generico',
-        },
-        {
-        value: 'referencia',
-        label: 'Referencia',
-        },
-        {
-        value: 'similar',
-        label: 'Similar',
-        },
-        {
-        value: 'etico',
-        label: 'Etico',
-        },
-        {
-        value: 'outros',
-        label: 'Outros',
-        },
-]
+      value: 'generico',
+      label: 'Generico',
+    },
+    {
+      value: 'referencia',
+      label: 'Referencia',
+    },
+    {
+      value: 'similar',
+      label: 'Similar',
+    },
+    {
+      value: 'etico',
+      label: 'Etico',
+    },
+    {
+      value: 'outros',
+      label: 'Outros',
+    },
+  ]
 
-        
 
-    return(
-        <div>
-          <Menu/>
-          <MainStyled>
+
+  return (
+    <div>
+      <Menu />
+      <MainStyled>
         <form
-        onSubmit={handleSubmit}>
-        {
-          inputFieldsRequired.map(({ label, name, id,value,handle,error}) => {
-            return (
-              <TextField margin="normal" onChange={handle} value={value} error={Boolean(error)} helperText={error} key={name} required id={id} label={label} name={name} autoFocus  color='success'
-              sx={{
-                margin: '1rem'
-              }} />)
-          })}
+          onSubmit={handleSubmit}>
+          {
+            inputFieldsRequired.map(({ label, name, id, value, handle, error }) => {
+              return (
+                <TextField margin="normal" onChange={handle} value={value} error={Boolean(error)} helperText={error} key={name} required id={id} label={label} name={name} autoFocus color='success'
+                  sx={{
+                    margin: '1rem'
+                  }} />)
+            })}
 
-<TextField
-          id="outlined-select-currency"
-          select
-          label="Tipo de medicamento"
-          helperText="Selecione o tipo de medicamento"
-          defaultValue={""}
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Tipo de medicamento"
+            helperText="Selecione o tipo de medicamento"
+            defaultValue={""}
             onChange={handleType}
             color="success"
             sx={{
-                margin: '1rem',
+              margin: '1rem',
             }}
-        
-        >
- {typeMedications.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
 
-        </TextField>
+          >
+            {typeMedications.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
 
-          <TextField margin="normal" onChange={handleDescription} value={description} id="descrition" multiline label={'Descrição'} color="success"  sx={{
+          </TextField>
+
+          <TextField margin="normal" onChange={handleDescription} value={description} id="description" multiline label={'Descrição'} color="success" sx={{
             margin: '1rem',
-            width:'90%'
+            width: '90%'
 
           }} ></TextField>
 
-      
 
 
-        <Button  variant="outlined" color='success' type="submit" sx={{
-          margin: '1rem',
-          
-        }}>Cadastrar</Button>
+
+          <Button variant="outlined" color='success' type="submit" sx={{
+            margin: '1rem',
+
+          }}>Cadastrar</Button>
 
         </form>
-    
+
 
       </MainStyled>
-        </div>
-    )
+    </div>
+  )
 }
-export {MedicationRegistry}
+export { MedicationRegistry }
