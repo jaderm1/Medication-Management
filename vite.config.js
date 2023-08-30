9832a36e1dd60225680e38a5b5b3b519c4b5d9fd
Jaderm1 <jadermaciel1@gmail.com>
2023-08-30 19:54:49 -0300

    fix deploy 01

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    rewrites: [
+      { source: "src/pages/map.jsx/", destination: "/map" },
+      {source: 'src/pages/medication-list.jsx', destination :'/medication-list'},
+      {source: 'src/pages/medication-registry.jsx', destination :'//medication-registry'},
+      {source: 'src/pages/pharmacy-registry.jsx', destination :'/pharmacy-registry'}
+    ]
+  },
 })
